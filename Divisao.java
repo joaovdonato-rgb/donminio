bd0ae171bcea9f50eb6966ac44b7474b960ddffb
joaovdonato-rgb <joaovdonato92donato@gmail.com>
2026-04-16 00:30:10 +0000

    AulaJava

---
+++ Divisao.java
@@ -0,0 +1,9 @@
+void main () {
+
+    float n1= Float.parseFloat(IO.readln("Digite o primeiro número: "));
+    float n2= Float.parseFloat(IO.readln("Digite o segundo número: "));
+
+float divisao = n1 / n2;
+
+IO.println("A divisão é: " + divisao);
+}
---
+++ Multiplica.java
@@ -0,0 +1,9 @@
+void main () {
+
+int n1 = Integer.parseInt(IO.readln("Digite o primeiro número: "));
+int n2 = Integer.parseInt(IO.readln("Digite o segundo número: "));
+
+int multip = n1 * n2;  
+
+IO.println("A multiplicação é: " * multip);
+}
